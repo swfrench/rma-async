d66540cdc78fac358480c027e0a3e78884c1311a
swfrench <scott.w.french@gmail.com>
2015-02-27 22:39:19 -0800

    Updated example

--- example.cpp
+++ example.cpp
@@ -11,12 +11,6 @@ func0()
 }
 
 void
-func1(int x)
-{
-  printf("[%s] x = %i\n", __func__, x);
-}
-
-void
 func2(int x, int y)
 {
   printf("[%s] x + y = %i\n", __func__, x + y);
@@ -35,25 +29,32 @@ main(int argc, char **argv)
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   async_enable(MPI_COMM_WORLD);
-  handle_t h1, h2;
+
   async((rank + 1) % size, func0);
-  async((rank + 1) % size, func1, 1);
+
   async((rank + 1) % size, func2, 1, 2);
+
+  MPI_Barrier(MPI_COMM_WORLD); // this is safe to do if you want
+
   async((rank + 1) % size, [rank] () {
     printf("i'm a lambda shipped from rank %i!\n", rank);
   });
-  h1 = async_handle((rank + 1) % size, [rank] () {
+
+  handle_t h1 = async_handle((rank + 1) % size, [rank] () {
     printf("i'm a lambda shipped from rank %i, but i have to happen first!\n",
            rank);
   });
-  h2 = async_chain(h1, (rank + 1) % size, [rank,h1] () {
+
+  handle_t h2 = async_chain(h1, (rank + 1) % size, [rank,h1] () {
     printf("i'm a lambda shipped from rank %i, and i'm running after %lli!\n",
            rank, h1);
   });
+
   async_after(h2, (rank + 1) % size, [rank,h2] () {
     printf("i'm a lambda shipped from rank %i, and i'm running after %lli!\n",
            rank, h2);
   });
+
   async_disable();
 
   MPI_Finalize();
